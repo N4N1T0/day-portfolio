550866d36583b2031fe6c8622b3bf3b7cc1dc1af
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-09-29 18:18:04 +0200

    feat: Add new Lint and Formatting

---
+++ .prettierrc.mjs
@@ -0,0 +1,18 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+        singleQuote: true,
+        trailingComma: "none",
+        tabWidth: 2,
+        semi: false,
+        jsxSingleQuote: true,
+      },
+    },
+  ],
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
-    integrations: [
-        mdx(),
-        sitemap(),
-        tailwind({
-            applyBaseStyles: false
-        })
-    ]
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    alpinejs(),
+  ],
 });
