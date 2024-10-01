922e6919efa4a336466778592499a8ce2d41e628
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-10-01 11:38:28 +0200

    feat: Add newsletter endpoint

--- src/pages/api/subscriber.json.ts
+++ src/pages/api/subscriber.json.ts
@@ -1,12 +1,62 @@
+import { prisma } from "@/prisma/client";
 import type { APIRoute } from "astro";
+
+export const prerender = false;
 
 export const POST: APIRoute = async ({ request }) => {
   const body = await request.json();
+  const email: string = body.email;
 
-  console.log(body);
+  if (!email) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: "Email is required, if you want to subscribe!",
+      }),
+      { status: 400 },
+    );
+  }
+
+  // Verifiy if the email is already subscribed
+  const existingSubscriber = await prisma.subscriber.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  if (existingSubscriber) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message:
+          "Email already subscribed, dont worry you will be updated soon!",
+      }),
+      { status: 409 },
+    );
+  }
+
+  // Create a new subscriber
+  const response = await prisma.subscriber.create({
+    data: {
+      email: email,
+    },
+  });
+
+  if (!response) {
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: "Error creating subscriber, try again!",
+      }),
+      { status: 500 },
+    );
+  }
+
   return new Response(
     JSON.stringify({
-      greeting: "Hello",
+      success: true,
+      message: "Subscriber created, dont worry you will be updated soon!",
     }),
+    { status: 201 },
   );
 };
