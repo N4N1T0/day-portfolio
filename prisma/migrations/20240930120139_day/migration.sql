2fcb4a443debea72de75bb8b4a99e43eaec0d41a
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-09-04 14:45:09 +0200

    chore: remove prisma-related files and migrations

--- prisma/migrations/20240930120139_day/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Subscriber" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Subscriber_pkey" PRIMARY KEY ("id")
-);
